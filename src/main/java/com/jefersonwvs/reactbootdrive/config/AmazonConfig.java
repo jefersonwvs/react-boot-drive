7375a27a4e036671cfdfedf78754cdb48759477a
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-02-20 16:23:12 -0400

    add s3 client builder

---
+++ src/main/java/com/jefersonwvs/reactbootdrive/config/AmazonConfig.java
@@ -0,0 +1,34 @@
+package com.jefersonwvs.reactbootdrive.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+	@Value("${awsAccessKey}")
+	private String accessKey;
+
+	@Value("${awsSecretAccessKey}")
+	private String secretAccessKey;
+
+	@Bean
+	public AmazonS3 s3() {
+
+		AWSCredentials awsCredentials = new BasicAWSCredentials(
+				this.accessKey,
+				this.secretAccessKey
+		);
+
+		return AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+				.build();
+
+	}
+}
