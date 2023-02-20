847f92882313f0e2070a5421b39b16d7316eae94
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-02-20 15:30:04 -0400

    project created

---
+++ src/main/java/com/jefersonwvs/reactbootdrive/ReactBootDriveApplication.java
@@ -0,0 +1,13 @@
+package com.jefersonwvs.reactbootdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBootDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBootDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefersonwvs/reactbootdrive/ReactBootDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefersonwvs.reactbootdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBootDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
