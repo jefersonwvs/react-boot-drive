7f6e39ff6708c16a261ba3c42267e4f7c850fc95
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-02-23 10:44:39 -0400

    implemented get user profiles logic on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function UserProfiles() {
+	const fetchUserProfiles = () => {
+		axios.get('http://localhost:8080/api/v1/user-profile').then((response) => {
+			console.log(response);
+		});
+	};
+
+	useEffect(() => {
+		fetchUserProfiles();
+	}, []);
+
+	return <h1>Hello</h1>;
+}
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<UserProfiles />
+		</div>
+	);
 }
 
 export default App;
